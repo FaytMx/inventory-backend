6f5aeea24cd95b0641ea9acb263d7f0933acee38
FaytMX <silverzero55@gmail.com>
2023-03-17 21:36:34 -0600

    proyecto base control de inventario

---
+++ src/test/java/cc/faytmx/springboot/app/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.faytmx.springboot.app.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
